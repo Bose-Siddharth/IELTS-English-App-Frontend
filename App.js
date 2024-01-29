76b7d261dd1343586bd2d069ee2cc34d73b5d3cd
Khatoon-Zahida <khatoonzoya39@gmail.com>
2024-01-29 17:45:07 +0530

    Add bottom tab, for navigation b/w login and signup screen

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Login" component={Login} />
+      <Tab.Screen name="Signup" component={Signup} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App;
---
+++ Pages/Signup.js
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+function Signup() {
+    return (
+        <Text>Login</Text>
+    )
+}
+
+export default Signup;
